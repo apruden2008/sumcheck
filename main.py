e2e9c46d6a8a3d194655f620685c34733567c05f
Alex Pruden <apruden2008@gmail.com>
2024-08-12 15:05:01 -0600

    clean up and document the main script

--- main.py
+++ main.py
@@ -5,61 +5,45 @@ from prover import Prover
 
 def main():
 
-# TODO At some point, make this a flag so you can actually input field elements if you want
-    # Take user input for the first field element
+    # Get user input for the prime field, and cast that value into an integer
     field_modulus = int(input("Enter a value for the field modulus that the polynomial is defined over: "))
-    term_count = int(input("How many terms is the polynomial comprised of?: "))
+
+    # Query the user for the number of total terms (non-constant) in the polynomial
+    term_count = int(input("How many non-constant terms is the polynomial comprised of?: "))
+
+    # Query the user for a constant term, if it exists
     poly_const = int(input("What is the constant term? If none, input '0': "))
-    poly_coeffs = [poly_const]
-    poly_powers = []
+
+    # Initialize a list for polynomial terms
     poly_terms = []
-    
+
+    # Iterate through the (non-constant) terms of the polynomial and convert the coefficients to field elements
+    # Then, convert those field elements & exponents into a full term, and append to the poly_terms list
     for i in range(term_count):
+
+        # Query the user for the coefficient of the ith term of the polynomial
         term_coeff = int(input("What is the value of the coefficient for the first term: "))
+
+        # Query the user for the exponent, or degree, of the ith term
         term_power = int(input("What is the degree of the variable in this term: "))
+
+        # Convert the coefficient to a field element
         fe = FieldElement(term_coeff, field_modulus)
+
+        # Create a Term object with the coefficient, variable number, and exponent/power
         pt = Term(fe, 1, term_power)
+
+        # Add the terms to the list defined above
         poly_terms.append(pt)
     
-    # Add the constant term
+    # Add the constant term to the poly_terms list
     poly_terms.append(Term(FieldElement(poly_const, field_modulus), 0, 0))
 
+    # Generate the Polynomial object from the poly_terms list
     poly = Polynomial(poly_terms)
+
+    # Print the polynomial string representation to the console
     print(str(poly))
-
-    # value1 = int(input("Enter the value for the first field element: "))
-    # modulus1 = int(input("Enter the modulus for the first field element: "))
-    
-    ## Create the first field element
-    #fe1 = FieldElement(value1, modulus1)
-    #
-    ## Take user input for the second field element
-    #value2 = int(input("Enter the value for the second field element: "))
-    #modulus2 = int(input("Enter the modulus for the second field element: "))
-    #
-    ## Create the second field element
-    #fe2 = FieldElement(value2, modulus2)
-    #
-    ## Check if both field elements are in the same field (same modulus)
-    #if modulus1 != modulus2:
-    #    print("Error: The moduli for the two field elements must be the same.")
-    #    return
-    #
-    ## Perform the addition operation
-    #result = fe1 + fe2
-    #
-    # Print the result
-    # print(f"The result of adding {fe1} and {fe2} is: {result}")
-
-    # Take user input for polynomial degree
-
-    # print("testing prover methods\n")
-    # test_prover = Prover(test_poly)
-    # claimed_value = test_prover.evaluate_polynomial(4) 
-    # print(claimed_value)
-    # hypercube_points = test_prover.get_hypercube_points()
-    # for point in hypercube_points:
-    #     print(point)
 
 if __name__ == '__main__':
     main()
