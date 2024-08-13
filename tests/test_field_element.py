0744fcb77163615acedaa75a82c9ea6dbc61ee3e
Alex Pruden <apruden2008@gmail.com>
2024-08-13 11:22:46 -0600

    add a few more tests for field element operations

--- tests/test_field_element.py
+++ tests/test_field_element.py
@@ -2,7 +2,28 @@ import unittest
 from field_element.field_element import FieldElement
 
 class TestFieldElement(unittest.TestCase):
+
+    def test_create(self):
+        # Create a basic field element consisting of a value and a prime modulus
+        value = 4
+        prime = 7
+        fe = FieldElement(value, prime)
+        self.assertEqual(fe, FieldElement(4,7))
+        self.assertEqual(fe.value, value)
+        self.assertEqual(fe.prime, prime)
+
+        # Create a field element greater than the prime modulus (should fail)
+        value = 25
+        prime = 23
+        with self.assertRaises(AssertionError):
+            FieldElement(value, prime)
     
+        # Create a field element with a nonprime modulus
+        value = 36
+        prime = 40
+        with self.assertRaises(AssertionError):
+            FieldElement(value, prime)
+
     def test_addition(self):
         # Basic Test
         fe1 = FieldElement(1, 7)
@@ -25,5 +46,11 @@ class TestFieldElement(unittest.TestCase):
         self.assertEqual(result, FieldElement(0, 13))
         print(str(result))
         
+        # Try adding elements from two different fields; should fail
+        fe1 = FieldElement(6,13)
+        fe2 = FieldElement(7,43)
+        with self.assertRaises(AssertionError):
+            fe1 + fe2
+
 if __name__ == '__main__':
     unittest.main()
