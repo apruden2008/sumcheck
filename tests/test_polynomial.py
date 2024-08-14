d4d13535bfb6956143148750f08bdfaefa2f424b
Alex Pruden <apruden2008@gmail.com>
2024-08-14 15:51:09 -0600

    debugging polynomial evaluation

--- tests/test_polynomial.py
+++ tests/test_polynomial.py
@@ -17,12 +17,13 @@ class TestPolynomial(unittest.TestCase):
                       Term(test_field_elements[1], 1, 1),
                       Term(test_field_elements[2], 2, 1),
                       Term(test_field_elements[3], 3, 1)]
-        test_points_vector = [2,2,3]
+        test_points_vector = [2,2,2]
         test_poly = Polynomial(test_terms)
+        # 1 + 1(2) + 2(2) + 3(2)
         # pdb.set_trace()
         result = test_poly.evaluate(test_points_vector)
         self.assertEqual(result, FieldElement(3, 7))
-        # print(str(result))
+        print(str(result))
 
     def test_degree(self):
         test_modulus = 7
@@ -38,6 +39,22 @@ class TestPolynomial(unittest.TestCase):
         result = test_poly.degree()
         self.assertEqual(result, 3)
         # print(str(result))
+
+    def test_zero_evaluation(self):
+        test_modulus = 13
+        test_field_elements = [FieldElement(4, test_modulus),
+                               FieldElement(12, test_modulus),
+                               FieldElement(3, test_modulus)]
+        test_constant_term = FieldElement(5, test_modulus)
+        # Term(coefficient, var_number, var_power)
+        test_terms = [Term(test_constant_term, 0, 1),
+                      Term(test_field_elements[0], 1, 3), 
+                      Term(test_field_elements[1], 2, 1),
+                      Term(test_field_elements[2], 3, 1)]
+        test_points_vector = [0,0,0]
+        test_poly = Polynomial(test_terms)
+        result = test_poly.evaluate(test_points_vector)
+        self.assertEqual(result, test_constant_term)
 
 if __name__ == '__main__':
     unittest.main()
