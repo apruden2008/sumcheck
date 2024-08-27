56f68803e522c893e0a1b46b613c2dee958cf360
Alex Pruden <apruden2008@gmail.com>
2024-08-27 16:26:35 -0600

    start verifier logic

---
+++ verifier/verifier.py
@@ -0,0 +1,14 @@
+from field_element.field_element import FieldElement
+from term.term import Term
+from polynomial.polynomial import Polynomial
+from prover.prover import Prover
+
+class Verifier:
+    def __init__(self):
+        """
+        Initialize the verifier 
+        """
+
+    def save_sum(claimed_sum):
+        return claimed_sum
+
